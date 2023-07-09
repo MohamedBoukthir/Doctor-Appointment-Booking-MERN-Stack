eef42eb919c72d72495eaf60bad0290c689803e9
MohamedBoukthir <medboukthir7@gmail.com>
2023-07-09 08:53:34 +0100

    adding navbar

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Header from '../components/Header'
+import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Routers from '../routes/Routers'
 
