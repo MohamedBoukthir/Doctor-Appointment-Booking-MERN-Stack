6a46b374b3929ec77f879fafdf05903e276ac2d8
MohamedBoukthir <medboukthir7@gmail.com>
2023-07-11 08:30:10 +0100

    add sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,10 @@ import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
 
+import featureImg from '../assets/images/feature-img.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
+
 import { Link } from 'react-router-dom'
 import { BsArrowRight} from 'react-icons/bs'
 import About from '../components/About/About'
@@ -181,7 +185,7 @@ const Home = () => {
 
       <About/>
 
-      {/* About section */} 
+      {/* Services section */} 
 
       <section>
         <div className="container">
@@ -197,6 +201,69 @@ const Home = () => {
         </div>
       </section>
 
+      {/* Feature section */} 
+
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+
+            <div className='xl:w-[670px]'>
+              <h2 className="heading">
+                Get Virtual Treatment <br /> Anytime.
+              </h2>
+
+              <ul className="pl-4">
+                <li className="text__para">
+                  1. Schedule The Appointment Directly.
+                </li>
+                <li className="text__para">
+                  2. Search for Your Physician Here and Contact Their Office.
+                </li>
+                <li className="text__para">
+                  3. View Our Physicians Who Are Accepting New Patients,
+                   Use the Online Scheduling Tool to Select an Appointment Time.
+                </li>
+              </ul>
+            <Link to='/'>
+              <button className="btn">Learn More</button>
+            </Link>
+            </div>
+
+            <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
+              <img src={featureImg} className='w-3/4' alt="" />
+
+              <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
+              md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                      Tue , 24
+                    </p>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]'>
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+              <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px]
+               leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full'>
+                Consultation 
+              </div>
+
+              <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                <img src={avatarIcon} alt="" />
+                <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700]
+                 text-headingColor'>Mohamed Boukthir</h4>
+              </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
