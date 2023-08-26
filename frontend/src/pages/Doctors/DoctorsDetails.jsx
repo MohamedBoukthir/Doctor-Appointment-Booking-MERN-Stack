4676f50eab60e2cc379521ec6d7a067c80b5f3df
MohamedBoukthir <medboukthir7@gmail.com>
2023-08-26 12:55:23 +0100

    SidePanel Section

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
 import DoctorAbout from './DoctorAbout'
 import Feedback from './Feedback'
+import SidePanel from './SidePanel'
 
 const DoctorsDetails = () => {
 
@@ -57,7 +58,9 @@ const DoctorsDetails = () => {
             </div>
           </div>
 
-          <div></div>
+          <div>
+            <SidePanel/>
+          </div>
         </div>
       </div>
     </section>
---
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -0,0 +1,51 @@
+
+const SidePanel = () => {
+  return (
+    <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
+        <div className="flex items-center justify-between">
+            <p className='text__para mt-0 font-semibold'>
+                Ticket Price
+            </p>
+            <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
+                500 TND
+            </span>
+        </div>
+        <div className='mt-[30px]'>
+            <p className='text__para mt-0 font-semibold text-headingColor'>
+                Available Time Slots:
+            </p>
+            <ul className='mt-3'>
+                <li className='flex items-center justify-between mb-2'>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        Sunday
+                    </p>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        5:00 PM : 9:30 PM
+                    </p>
+                </li>
+                <li className='flex items-center justify-between mb-2'>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        Tuesday
+                    </p>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        5:00 PM : 9:30 PM
+                    </p>
+                </li>
+                <li className='flex items-center justify-between mb-2'>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        Friday
+                    </p>
+                    <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                        5:00 PM : 9:30 PM
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <button className='btn px-2 w-full rounded-md'>
+            Book
+        </button>
+    </div>
+  )
+}
+
+export default SidePanel
