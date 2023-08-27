1ad1cea4bbb226130291e5eb0eda1e50fe2c4a00
MohamedBoukthir <medboukthir7@gmail.com>
2023-08-27 13:00:11 +0100

    Sidepanel component

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -1,7 +1,7 @@
 
 const SidePanel = () => {
   return (
-    <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
+    <div className='shadow p-3 lg:p-5 rounded-md'>
         <div className="flex items-center justify-between">
             <p className='text__para mt-0 font-semibold'>
                 Ticket Price
