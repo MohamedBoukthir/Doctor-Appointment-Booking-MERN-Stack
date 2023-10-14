3181386d019dd5fd7d080a59e004ad700cd1d285
Sam <learnwithfun4567@gmail.com>
2023-10-14 12:44:27 +0530

    done upto 35:42 / 1:31:31 (Part-4)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthContextProvider } from "./context/authContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,60 @@
-import { useState } from "react"
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../config";
+import { toast } from "react-toastify";
+import { authContext } from "../context/authContext.jsx";
+import { useContext } from "react";
 const Login = () => {
-
   const [formData, setFormData] = useState({
-    email:'',
-    password: ''
+    email: "",
+    password: "",
   });
 
-  const handleInputChange = e => {
-    setFormData({...formData,[e.target.name]: e.target.value});
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { dispatch } = useContext(authContext);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/api/v1/auth/login`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        throw new Error(result.message);
+      }
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, "login data");
+
+      setLoading(false);
+      toast.success(result.message);
+      navigate("/home");
+    } catch (err) {
+      toast.error(err.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -18,44 +63,47 @@ const Login = () => {
         <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
           Hello! <span className="text-primaryColor">Welcome</span> Back
         </h3>
-        <form className="py-4 md:py-0">
+        <form className="py-4 md:py-0" onSubmit={submitHandler}>
           <div className="mb-5">
-            <input type="email"
-            placeholder="Enter Your Email"
-            name="email"
-            value={formData.email} 
-            onChange={handleInputChange}
-            className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
-            required
-          />
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+              required
+            />
           </div>
           <div className="mb-5">
-            <input type="password"
-            placeholder="Password Here"
-            name="password"
-            value={formData.password} 
-            onChange={handleInputChange}
-            className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
-            required
-          />
+            <input
+              type="password"
+              placeholder="Password Here"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+              required
+            />
           </div>
           <div className="mt-7">
             <button
               type="submit"
-              className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3">
+              className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
+            >
               Login
             </button>
           </div>
           <p className="mt-5 text-textColor text-center">
-            Don&apos;t have an account? 
-              <Link to='/register' className='text-primaryColor font-medium ml-1'>
-                 Register
-              </Link>
+            Don&apos;t have an account?
+            <Link to="/register" className="text-primaryColor font-medium ml-1">
+              Register
+            </Link>
           </p>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
